b15678755446359a77eb6ff8aedd555068f4fe5f
Q <justin.quinton@mitel.com>
2018-02-20 12:48:29 -0500

    Added missed import

--- android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
+++ android/src/main/java/com/wix/reactnativenotifications/RNNotificationsPackage.java
@@ -3,6 +3,7 @@ package com.wix.reactnativenotifications;
 import android.app.Application;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
